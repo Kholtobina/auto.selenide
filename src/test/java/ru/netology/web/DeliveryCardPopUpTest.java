fafb6b620adef62a14f874aab451eede62bfeb2f
Kholtobina <another.z@rambler.ru>
2020-10-28 16:42:12 +0200

    Initial commit

---
+++ src/test/java/ru/netology/web/DeliveryCardPopUpTest.java
@@ -0,0 +1,79 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DeliveryCardPopUpTest {
+
+    @Test
+    void shouldTestCityList() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Як");
+        $$(".menu-item .menu-item__control").find(exactText("Брянск")).click();
+
+        $("[data-test-id='city'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='city'] input").setValue("Як");
+        $$(".menu-item .menu-item__control").find(exactText("Красноярск")).click();
+
+        $("[data-test-id='city'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='city'] input").setValue("Як");
+        $$(".menu-item .menu-item__control").find(exactText("Ульяновск")).click();
+
+        $("[data-test-id='city'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='city'] input").setValue("Як");
+        $$(".menu-item .menu-item__control").find(exactText("Челябинск")).click();
+
+        $("[data-test-id='city'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='city'] input").setValue("Як");
+        $$(".menu-item .menu-item__control").find(exactText("Якутск")).click();
+
+    }
+
+    @Test
+    void shouldTestDateNextWeek() {
+        open("http://localhost:9999");
+        LocalDate currentDate = LocalDate.now();
+        LocalDate dateOfDelivery = LocalDate.now().plusDays(7);
+        String day = dateOfDelivery.format(DateTimeFormatter.ofPattern("d"));
+        $(".input__icon").click();
+       if (dateOfDelivery.getMonthValue() - currentDate.getMonthValue() == 1) {
+            $("[data-step='1']").click();
+        }
+        $$("td.calendar__day").find(exactText(day)).click();
+    }
+
+    @Test
+    void shouldTestCorrectForm() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Мо");
+        $$(".menu-item .menu-item__control").find(exactText("Москва")).click();
+
+        LocalDate currentDate = LocalDate.now();
+        LocalDate dateOfDelivery = LocalDate.now().plusDays(7);
+        String date = dateOfDelivery.format(DateTimeFormatter.ofPattern("d"));
+        $(".input__icon").click();
+        if (dateOfDelivery.getMonthValue() - currentDate.getMonthValue() == 1) {
+            $("[data-step='1']").click();
+        }
+        $$("td.calendar__day").find(exactText(date)).click();
+
+        $("[data-test-id='name'] input").setValue("Игорь Попов");
+        $("[data-test-id='phone'] input").setValue("+79012345678");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(byText("Успешно!")).waitUntil(visible, 11000);
+        $(byText("Встреча успешно забронирована на"));
+
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateDelivery = formatter1.format(dateOfDelivery);
+        $(byText(dateDelivery));
+    }
+}
